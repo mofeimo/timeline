634206bd221d45bf9fc0fa317ad813c4549fa848
MQQ <736747261@qq.com>
2022-04-21 09:55:19 +0800

    test

--- js/timeline.js
+++ js/timeline.js
@@ -1,14 +1,14 @@
-$(()=>{
-    // first we need to create a stage
-var stage = new Konva.Stage({
+$(() => {
+  // first we need to create a stage
+  var stage = new Konva.Stage({
     container: 'container',   // id of container <div>
     width: 500,
     height: 500
   });
-  
+
   // then create layer
   var layer = new Konva.Layer();
-  
+
   // create our shape
   var circle = new Konva.Circle({
     x: stage.width() / 2,
@@ -18,13 +18,13 @@ var stage = new Konva.Stage({
     stroke: 'black',
     strokeWidth: 4
   });
-  
+
   // add the shape to the layer
   layer.add(circle);
-  
+
   // add the layer to the stage
   stage.add(layer);
-  
+
   // draw the image
   layer.draw();
 });
