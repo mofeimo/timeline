78738a9999d2e35f282ad93b22832b00314e3586
MQQ <736747261@qq.com>
2023-02-27 17:33:30 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import VueKonva from "vue-konva"
+
 import App from './App.vue'
 import router from './router'
+import store from './store'
 
 import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(VueKonva)
 app.use(router)
+app.use(store)
+
+app.config.errorHandler=(err, vm, info)=>{
+    console.error(err);
+}
 
 app.mount('#app')
