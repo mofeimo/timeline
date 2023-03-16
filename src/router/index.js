f08f3f5a4b9d5cbca1036480a147fec54f7e7b5d
MQQ <736747261@qq.com>
2023-03-16 16:11:27 +0800

    build

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import IndexView from '../views/index.vue'
 
 let routes = []
@@ -16,7 +16,7 @@ routes.push({
 	})
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory(),
 	routes
 })
 
